25e4622051431b0d555e4b6cc388f2eb9243af76
pos0637 <pos0637@163.com>
2018-07-24 21:57:34 +0800

    使用ByteBuddy动态生成Controller代码。

--- research/test_auto_restful/src/main/java/com/furongsoft/openmes/research/test_auto_restful/RestfulControllerDefinitionRegistryPostProcessor.java
+++ research/test_auto_restful/src/main/java/com/furongsoft/openmes/research/test_auto_restful/RestfulControllerDefinitionRegistryPostProcessor.java
@@ -1,6 +1,7 @@
 package com.furongsoft.openmes.research.test_auto_restful;
 
-import com.itranswarp.compiler.JavaStringCompiler;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -10,10 +11,12 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
 public class RestfulControllerDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
@@ -36,34 +39,46 @@ public class RestfulControllerDefinitionRegistryPostProcessor implements BeanDef
         this.applicationContext = applicationContext;
     }
 
-    static final String JAVA_SOURCE_CODE = ""
-            + "package on.the.fly;                                            "
-            + "import org.springframework.stereotype.Controller;              "
-            + "import org.springframework.web.bind.annotation.RequestMapping; "
-            + "import org.springframework.web.bind.annotation.ResponseBody;   "
-            + "@Controller                                                    "
-            + "public class Foo {                                             "
-            + "    @RequestMapping(\"/bar\")                                  "
-            + "    @ResponseBody                                              "
-            + "    public String bar() {                                      "
-            + "        return \"bar\";                                        "
-            + "    }                                                          "
-            + "}";
+    public class BaseController {
+        @RequestMapping("/bar")
+        @ResponseBody
+        public String bar() {
+            return "bar";
+        }
+    }
+
+    class ControllerImpl implements Controller {
+        @Override
+        public String value() {
+            return "";
+        }
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return Controller.class;
+        }
+    }
 
     private void generateBean(BeanDefinitionRegistry beanDefinitionRegistry, List<BeanDefinition> beanDefinitions) {
-        JavaStringCompiler compiler = new JavaStringCompiler();
-        try {
-            Map<String, byte[]> results = compiler.compile("Foo.java", JAVA_SOURCE_CODE);
-            Class<?> clazz = compiler.loadClass("on.the.fly.Foo", results);
+        if (beanDefinitions == null) {
+            return;
+        }
+
+        beanDefinitions.forEach(beanDefinition -> {
+            Class<?> clazz = new ByteBuddy()
+                    .subclass(BaseController.class)
+                    .annotateType(new ControllerImpl())
+                    .name(beanDefinition.getBeanClassName())
+                    .make()
+                    .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+                    .getLoaded();
 
             GenericBeanDefinition definition = new GenericBeanDefinition();
             definition.setBeanClass(clazz);
             definition.setScope("singleton");
             definition.setLazyInit(false);
             definition.setAutowireCandidate(true);
-            beanDefinitionRegistry.registerBeanDefinition("Foo", definition);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+            beanDefinitionRegistry.registerBeanDefinition(beanDefinition.getBeanClassName(), definition);
+        });
     }
 }
